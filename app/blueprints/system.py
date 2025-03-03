60c6de4e397ca307c43596ac3253a8805159889f
olavkhn <o.kuhnen@leadinnovation.nl>
2025-03-03 22:21:05 +0100

    Automatic push (2025-03-03 22:21:05)

--- app/blueprints/system.py
+++ app/blueprints/system.py
@@ -1,5 +1,3 @@
-import logging
-
 from flask import Blueprint, jsonify, request
 
 from app.config import Config
@@ -49,26 +47,19 @@ def api_network():
 def process_env():
     data = request.get_json()
 
-    # Validate request structure
     if not data or "auth_token" not in data or "env_data" not in data:
         return jsonify({"error": "Invalid request format"}), 400
 
     auth_token = data["auth_token"]
     env_data = data["env_data"]
 
-    # Authenticate the request
     if auth_token != Config.AUTH_SHIP_ENV_TOKEN:
-        logging.debug(
-            f"Auth_token: {auth_token}\nAUTH_SHIP_TOKEN: {Config.AUTH_SHIP_ENV_TOKEN}"
-        )
         return jsonify({"error": "Unauthorized"}), 403
 
     try:
-        # Convert the dictionary into a .env format
         env_lines = [f"{key}={value}" for key, value in env_data.items()]
         env_content = "\n".join(env_lines)
 
-        # Write the new environment variables to the .env file
         with open(".env", "w") as env_file:
             env_file.write(env_content)
 
