6c75dff91ed91959a80a46bc3ae934629a3ffd45
olavkhn <o.kuhnen@leadinnovation.nl>
2025-03-03 19:55:33 +0100

    Automatic push (2025-03-03 19:55:32)

--- run.py
+++ run.py
@@ -7,15 +7,5 @@ log.setLevel(logging.ERROR)
 
 app = create_app()
 
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
-
-    """
-    Add ssl_context=('cert.pem', 'key.pem') to app.run to turn on SSL usage (HTTPS)
-    """
-
-
-# Test comment
-# Test 2
-# Test 3
+# To run the project, execute
+# gunicorn -w 2 -b 0.0.0.0:5000 run:app
