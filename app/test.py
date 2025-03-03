cdc17c4386d7d3a9d938627a1b593fcb1e2ee6fe
olavkhn <o.kuhnen@leadinnovation.nl>
2025-03-03 19:35:01 +0100

    Automatic push (2025-03-03 19:35:01)

---
+++ app/test.py
@@ -0,0 +1,9 @@
+import datetime
+import os
+import sys
+import time
+import turtle
+
+import pyautogui
+
+print("HEllo world!")
---
+++ scripts/format.ps1
@@ -0,0 +1,17 @@
+# This format.ps1 file acts like a simple way of applying style-related rules
+# This script will be ran when running ./push,ps1
+# The script can also be ran by hand by doing ./format.ps1
+
+
+# Isort automatically sorts and organizes all import statements in this project.
+# it keeps the imports consistent, organized, and avoids duplicate or unordered imports
+isort . *> $null
+
+
+# Automatically formats Python code to follow the PEP 8 style guide.
+# Ensures consistent code formatting, so you don’t have to worry about spacing, line breaks, or indentation.
+black . *> $null
+
+#  Checks Python code for errors, unused variables, and bad practices (linting).
+#  Helps catch syntax errors, missing imports, bad indentation, and unused variables.
+flake8 . --ignore E501 > $null
